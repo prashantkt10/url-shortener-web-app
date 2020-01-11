31bd860ab5b2a0b4ed20c1f65999cee509b1fe56
Prashant Kumar Tripathi <hello@prashant.live>
2020-01-11 16:20:03 +0530

    more work

--- router/api/otp/setotp.js
+++ router/api/otp/setotp.js
@@ -17,19 +17,22 @@ router.post('/', [
             var sendEmail = async function (data) { if (!data) return; return await emailAccount.sendMail(data); }
             const otp = genOTP();
             redisClient.set(dbRes.rows[0]['email'], otp, function (err, data) {
+                console.log('step0');
                 if (err || !data) return res.status(500).json({ message: 'System error0' });
                 redisClient.expire(dbRes.rows[0]['email'], 60, async function (err, data) {
+                    console.log('step1');
                     if (err || !data) return res.status(500).json({ message: 'System error1' });
                     await sendEmail({
                         from: 'dwarfme@prashant.live', to: email, subject: "OTP request for DwarfME",
                         html: `<p>Dear User,</p><p>OTP to reset DwarfMe account is: <b>${otp}</b></p><br/><p><b>Best Regards,<br/>DwarfMe.</b><p>`
                     }).then((data) => {
+                        console.log('step2');
                         if (!data || !data.accepted || !data.accepted.length) {
                             redisClient.del(dbRes.rows[0]['email'], function (err, data) {
                                 if (err || !data) return res.status(500).json({ message: 'System error2' });
                             });
                         } else { return res.json({ message: 'OTP sent' }); }
-                    }).catch((err) => { });
+                    }).catch((err) => { console.log('step3'); });
                 });
             });
         } else return res.status(400).json({ message: 'No email matched' });
